544018f68f602043b28038fd24c25b0fb4b13c8b
amorgrace <akanforte@gmail.com>
2025-02-16 19:22:13 +0100

    debug and hosts allowed

--- backend/final/settings.py
+++ backend/final/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-icr*&t=ixa+fj2cnn&q@d=f3v%gjqpwmw#2b+)jo=vtck)4wh)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 
